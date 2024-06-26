54e858ff36d3c58edbb12137962f3102a9f1ca9f
tbender2021 <143292915+tbender2021@users.noreply.github.com>
2024-06-26 16:52:20 -0400

    Setting up project
    Setting up basic file structure along with empty home and cart page plus a navbar component

---
+++ ecommerce-project/src/pages/shop/shop.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Shop = () => {
+    return(
+        <div className="shop">
+            <div classname = "shopTitle"> 
+                <h1>BenderStar Digital</h1>
+            </div>
+            <div className="products"></div>
+        </div>
+    );
+    }
